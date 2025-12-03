5b9a78d8e133f2ef8d335d6d78d3b0352dae2e1b
Lukalukito4321 <115914245+Lukalukito4321@users.noreply.github.com>
2025-12-03 21:44:57 +0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-async function load(){
-  const r = await fetch("/stats");
-  const j = await r.json();
-  document.getElementById("out").textContent = JSON.stringify(j,null,2);
-}
-setInterval(load,1000);
-load();
